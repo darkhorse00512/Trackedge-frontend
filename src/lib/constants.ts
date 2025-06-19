c1bad68d2b4247807548c3e9c09d15af2a5309ee
Darkhorse <horsedark374@gmail.com>
2025-06-19 18:15:31 -0500

    constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+
+export const CURRENCY_PAIRS = [
+  "EUR/USD",
+  "GBP/USD",
+  "USD/JPY",
+  "USD/CHF",
+  "USD/CAD",
+  "AUD/USD",
+  "NZD/USD",
+  "EUR/GBP",
+  "EUR/JPY",
+  "GBP/JPY",
+  "XAU/USD"
+];
