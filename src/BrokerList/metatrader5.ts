2a9cee9636d50d3a20b60614899d4bf4b72a2dfd
Darkhorse <horsedark374@gmail.com>
2025-06-19 18:13:58 -0500

    brokerlist

---
+++ src/BrokerList/metatrader5.ts
@@ -0,0 +1,41 @@
+
+
+
+
+export const listOfMetatrader5Brokers = [
+    // Pepperstone UK
+    'PepperstoneUK-Demo',
+    'PepperstoneUK-Live',
+    // Pepperstone (regular)
+    'Pepperstone-Demo',
+    'Pepperstone-MT5-Live01',
+    // IC Markets International
+    'ICMarketsInternational-Demo',
+    'ICMarketsInternational-MT5',
+    'ICMarketsInternational-MT5-2',
+    'ICMarketsInternational-MT5-4',
+    // IC Markets EU 
+    'ICMarketsEU-Demo',
+    'ICMarketsEU-MT5-5',
+    // IC Markets KE 
+    'ICMarketsKE-Demo',
+    'ICMarketsKE-MT5-7',
+    // IC Markets KY
+    'ICMarketsKY-Demo',
+    'ICMarketsKY-MT5-4',
+    // XM Global Limited
+    'XMGlobal-MT5',
+    'XMGlobal-MT5 10',
+    'XMGlobal-MT5 11',
+    'XMGlobal-MT5 12',
+    'XMGlobal-MT5 13',
+    'XMGlobal-MT5 14',
+    'XMGlobal-MT5 15',
+    'XMGlobal-MT5 2',
+    'XMGlobal-MT5 4',
+    'XMGlobal-MT5 5',
+    'XMGlobal-MT5 6',
+    'XMGlobal-MT5 7',
+    'XMGlobal-MT5 8',
+    'XMGlobal-MT5 9',
+];
